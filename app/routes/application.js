82820e8f6d41717a8552c7626b85470a49e43a2f
demo <demo@helloworld.local>
2018-04-20 13:20:48 +0200

    Edit updates done

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,7 +6,34 @@ export default Route.extend({
             notice: {
                 title: "Jetzt neu!",
                 message: "Das Info Display ist hier"
+            },
+            updates: [
+                {
+                    when: "dann",
+                    title: "findet",
+                    text: "etwas statt"
+                },
+                {
+                    when: "wann anders",
+                    title: "findet auch",
+                    text: "noch etwas anderes statt"
+                }
+            ]
+        }
+    },
+
+    actions: {
+        newUpdate(what, when, how) {
+            // console.log("neues update", what, when, how);
+            let model = this.modelFor('application').updates;
+            if (what && when && how) {
+                model.unshift({
+                    when: when,
+                    title: what,
+                    text: how
+                });
             }
+            
         }
     }
 });
