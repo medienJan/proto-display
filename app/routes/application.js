86bb6ba0a2894decafe6de68bddbea834da82e1c
medienJan <Jan.Figura@web.de>
2018-04-20 16:45:51 +0200

    Removed unnecessary action

--- app/routes/application.js
+++ app/routes/application.js
@@ -8,18 +8,5 @@ export default Route.extend({
         message: "Das Info Display ist hier"
       }
     }
-  },
-  actions: {
-    newUpdate(when, what, how) {
-      if (when && what && how) {
-        var model = this.modelFor(this.routeName);
-        model.updates.unshift({
-          when: when,
-          title: what,
-          text: how
-        });
-        console.log(model);
-      }
-    }
   }
 });
