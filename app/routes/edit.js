d0e9e0b8e7c166a2ce78b7ebfefc4d7ec1ccd72e
medienJan <Jan.Figura@web.de>
2018-04-20 17:08:25 +0200

    Removed changes

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({});
+export default Route.extend({
+  model() {
+    let model = this.store.findAll('update');
+    console.log(model);
+    return model;
+  }
+});
