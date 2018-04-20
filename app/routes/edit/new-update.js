9cf21cc7722f3e39c16c7d1c1c48cc2505314a7c
medienJan <Jan.Figura@web.de>
2018-04-20 16:02:48 +0200

    moved the new update action to the corresponding route

--- app/routes/edit/new-update.js
+++ app/routes/edit/new-update.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    async newUpdate(when, what, how) {
+      if (when && what && how) {
+        let model = await this.store.createRecord("update", {
+          when: when,
+          title: what,
+          text: how
+        });
+        model.save();
+        this.transitionTo('edit');
+      }
+    }
+  }
 });
