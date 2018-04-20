fa42cb4ddc9740a2b8857e1ec0ed5b083e99e367
demo <demo@helloworld.local>
2018-04-20 12:59:35 +0200

    Edit routes with edit notice

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('edit', function() {
+    this.route('notice');
+    this.route('newupdate');
+  });
 });
 
 export default Router;
