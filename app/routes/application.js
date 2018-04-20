c2dcad0561876cfdbf036ad14c53a9c49815af24
medienJan <Jan.Figura@web.de>
2018-04-20 14:41:17 +0200

    Moved the updates model to the to the main application model

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,23 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model() {
-        return {
-            notice: {
-                title: "Jetzt neu!",
-                message: "Das Info Display ist hier"
-            }
+  model() {
+    return {
+      notice: {
+        title: "Jetzt neu!",
+        message: "Das Info Display ist hier"
+      },
+      updates: [{
+          when: "dann",
+          title: "findet",
+          text: "etwas statt"
+        },
+        {
+          when: "wann anders",
+          title: "findet auch",
+          text: "noch etwas anderes statt"
         }
+      ]
     }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-    model() {
-        return {
-            updates: [
-                {
-                    when: "dann",
-                    title: "findet",
-                    text: "etwas statt"
-                },
-                {
-                    when: "wann anders",
-                    title: "findet auch",
-                    text: "noch etwas anderes statt"
-                }
-            ]
-        };
-    }
-});
+export default Route.extend({});
